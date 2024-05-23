21160af0ad4e52964521b7fac674e55306c31a7e
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-05-23 23:25:18 +0800

    hello, java

---
+++ src/main/java/week1/Welcome.java
@@ -0,0 +1,14 @@
+package week1;
+
+/**
+ * @author rd_qinglin_mu
+ * @description welcome
+ * @单据标识
+ * @date 2024/5/23 23:21
+ **/
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
