ce5a9e0feed06c96f991ced68ec4c56e293d24ae
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-05-25 17:38:55 +0800

    hasNext

---
+++ src/main/java/week1/JavaHasNext.java
@@ -0,0 +1,25 @@
+package week1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author rd_qinglin_mu
+ * @description end of file
+ * @单据标识
+ * @date 2024/5/25 15:26
+ **/
+public class JavaHasNext {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> results = new ArrayList<>();
+        // hasNext 会block函数，等待指令，要么输入东西，返回true，要么输入EOF返回false，就会执行下一段代码
+        while (scan.hasNext()) {
+            String str = scan.nextLine();
+            results.add(str);
+        }
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+}
