316d78369b4b3a2948d77306fc53c00beb880ee7
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-06-12 22:47:52 +0800

    ArrayList examples

---
+++ src/main/java/week4/List.java
@@ -0,0 +1,41 @@
+package week4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author rd_qinglin_mu
+ * @description list
+ * @单据标识
+ * @date 2024/6/12 21:28
+ **/
+public class List {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> results = new ArrayList<>();
+        int numberOfDigit = scan.nextInt();
+
+        for (int i = 0; i < numberOfDigit; i++) {
+            int currentNumber = scan.nextInt();
+            results.add(currentNumber);
+        }
+
+        int queries = scan.nextInt();
+        for (int i = 0; i < queries; i++) {
+            String operation = scan.next();
+            switch (operation) {
+                case "Insert":
+                    results.add(scan.nextInt(), scan.nextInt());
+                    break;
+                case "Delete":
+                    results.remove(scan.nextInt());
+                    break;
+            }
+        }
+
+        for (Integer result : results) {
+            System.out.print(result + " ");
+        }
+
+    }
+}
