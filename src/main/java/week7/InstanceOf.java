a5edae423ace7185b6ba33016f821113876e21a2
hi-jennie <hi-jennie@outlook.com>
2024-07-15 16:38:14 +0800

    typos

--- src/main/java/week7/InstanceOf.java
+++ src/main/java/week7/InstanceOf.java
@@ -54,10 +54,10 @@ public class InstanceOf {
 
 
 //public class InstanceOf {
-//    static String count(ArrayList mylist) {
+//    static String count(ArrayList myList) {
 //        int a = 0, b = 0, c = 0;
-//        for (int i = 0; i < mylist.size(); i++) {
-//            Object element = mylist.get(i);
+//        for (int i = 0; i < myList.size(); i++) {
+//            Object element = myList.get(i);
 //            if (element instanceof Student)
 //                a++;
 //            if (element instanceof Rockstar)
@@ -70,16 +70,16 @@ public class InstanceOf {
 //    }
 //
 //    public static void main(String[] argh) {
-//        ArrayList mylist = new ArrayList();
+//        ArrayList myList = new ArrayList();
 //        Scanner sc = new Scanner(System.in);
 //        int t = sc.nextInt();
 //        for (int i = 0; i < t; i++) {
 //            String s = sc.next();
-//            if (s.equals("Student")) mylist.add(new Student());
-//            if (s.equals("Rockstar")) mylist.add(new Rockstar());
-//            if (s.equals("Hacker")) mylist.add(new Hacker());
+//            if (s.equals("Student")) myList.add(new Student());
+//            if (s.equals("Rockstar")) myList.add(new Rockstar());
+//            if (s.equals("Hacker")) myList.add(new Hacker());
 //        }
-//        System.out.println(count(mylist));
+//        System.out.println(count(myList));
 //    }
 //
 //    static class Student {
